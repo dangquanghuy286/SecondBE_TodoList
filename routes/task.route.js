5805e8d65f1da01e80fcfb24321264d6a777166f
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-08-28 00:36:16 +0700

    feat: change-multi

--- routes/task.route.js
+++ routes/task.route.js
@@ -5,4 +5,5 @@ const taskController = require("../controllers/task.controller");
 router.get("/", taskController.index);
 router.get("/detail/:id", taskController.detail);
 router.patch("/change-status/:id", taskController.changeStatus);
+router.patch("/change-multi", taskController.changeMulti);
 module.exports = router;
